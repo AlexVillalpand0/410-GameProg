ffd9dac308a0bed710ed183f788fd8bb57b8eea6
AlexVillalpand0 <78947506+AlexVillalpand0@users.noreply.github.com>
2021-06-10 21:59:47 -0700

    Full Build

---
+++ 3D_Beginner/Library/Il2cppBuildCache/WebGL/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,163 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void GameEnding::OnTriggerEnter(UnityEngine.Collider)
+extern void GameEnding_OnTriggerEnter_m87780FB176345B677FBDCB1CF4C50A0888800471 (void);
+// 0x00000002 System.Void GameEnding::CaughtPlayer()
+extern void GameEnding_CaughtPlayer_mC2C71F5AC36218A37F16C993BD343829AE2F6132 (void);
+// 0x00000003 System.Void GameEnding::Update()
+extern void GameEnding_Update_m120E149C8CC72CE194DFB943A97709575958D142 (void);
+// 0x00000004 System.Void GameEnding::EndLevel(UnityEngine.CanvasGroup,System.Boolean,UnityEngine.AudioSource)
+extern void GameEnding_EndLevel_m07541912C8063B8BA6B15E19FF8E46EDB13B1594 (void);
+// 0x00000005 System.Void GameEnding::.ctor()
+extern void GameEnding__ctor_mDF775B6FCFF45EF72E175030518298389D957F34 (void);
+// 0x00000006 System.Void GhostTrack::Start()
+extern void GhostTrack_Start_mC81B9E80546FE659E2DC56C58139C183EE02CC23 (void);
+// 0x00000007 System.Void GhostTrack::FixedUpdate()
+extern void GhostTrack_FixedUpdate_mB44E5BC20A318EE485801E368CB674710A1DBBFA (void);
+// 0x00000008 System.Void GhostTrack::.ctor()
+extern void GhostTrack__ctor_m550A2D7DB5362A5D0F832515B6A5A5D8CEA54DF3 (void);
+// 0x00000009 System.Void LightFlicker::Start()
+extern void LightFlicker_Start_mC77FC1A4B785EAF0C92673DB81D126DC5E5A902D (void);
+// 0x0000000A System.Void LightFlicker::Update()
+extern void LightFlicker_Update_mB1460DCFEDEFED37E27D5B03DE530A8140028813 (void);
+// 0x0000000B System.Void LightFlicker::ChangeRandomFlickerLightIntensity()
+extern void LightFlicker_ChangeRandomFlickerLightIntensity_mB764800D45DAF80519D2D9DD24FF65D6D7124216 (void);
+// 0x0000000C System.Void LightFlicker::ChangeAnimatedFlickerLightIntensity()
+extern void LightFlicker_ChangeAnimatedFlickerLightIntensity_m49FFE0D736C10BFDE6B62771E57E8241883FAA5B (void);
+// 0x0000000D System.Void LightFlicker::.ctor()
+extern void LightFlicker__ctor_m5433F03E33D93405B63644FBF62E402BC57D61A1 (void);
+// 0x0000000E System.Void LightFlicker::.cctor()
+extern void LightFlicker__cctor_mE817BA31A2A18688C862C1FA1E2E0A500679B52B (void);
+// 0x0000000F System.Void Observer::OnTriggerEnter(UnityEngine.Collider)
+extern void Observer_OnTriggerEnter_m8A32D16C3C592D1CDF69B46AF25FFA5BDBB19B10 (void);
+// 0x00000010 System.Void Observer::OnTriggerExit(UnityEngine.Collider)
+extern void Observer_OnTriggerExit_mD852F0FAB4256C02B5680E1310DF350528110069 (void);
+// 0x00000011 System.Void Observer::Update()
+extern void Observer_Update_mD03987999F861A59B23749CDEA96596323F9D649 (void);
+// 0x00000012 System.Void Observer::.ctor()
+extern void Observer__ctor_m64049426C599F37763A505365E32A802FA64264F (void);
+// 0x00000013 System.Void PlayerMovement::Start()
+extern void PlayerMovement_Start_mB585552228B1908E44D3A69496598FB485F608B6 (void);
+// 0x00000014 System.Void PlayerMovement::FixedUpdate()
+extern void PlayerMovement_FixedUpdate_m774280268A537B6ED9D9171CEAE67E9A0C3A9499 (void);
+// 0x00000015 System.Void PlayerMovement::OnAnimatorMove()
+extern void PlayerMovement_OnAnimatorMove_m7913AC2E561ABF9136444B82FC7FA278F1EBD25A (void);
+// 0x00000016 System.Void PlayerMovement::.ctor()
+extern void PlayerMovement__ctor_mBF9F632DD9929DD6FF092A968649A4406BFE397F (void);
+// 0x00000017 System.Void Rotate::Start()
+extern void Rotate_Start_m260F3DA22D8BEE3571FB61596065ECC238B60968 (void);
+// 0x00000018 System.Void Rotate::FixedUpdate()
+extern void Rotate_FixedUpdate_mF0A3C36692B5A43C722469B4F0851FD87B471CCB (void);
+// 0x00000019 System.Void Rotate::.ctor()
+extern void Rotate__ctor_mAB2884DA9234D7A6485C5662D97205C92CA9B9C4 (void);
+// 0x0000001A System.Void WaypointPatrol::Start()
+extern void WaypointPatrol_Start_m497127FB0E2962E24BC18EADE7FA4DE6F80A6606 (void);
+// 0x0000001B System.Void WaypointPatrol::Update()
+extern void WaypointPatrol_Update_m4FCAE29E3B278293FE1984F50EDAF94169C37F8D (void);
+// 0x0000001C System.Void WaypointPatrol::.ctor()
+extern void WaypointPatrol__ctor_m52AC8FA137BDD414C942AFCD75E03D30D9BED6E2 (void);
+// 0x0000001D System.Void Readme::.ctor()
+extern void Readme__ctor_mF465410C5B2E598F2685E82CFCE1F42186AFF448 (void);
+// 0x0000001E System.Void Readme/Section::.ctor()
+extern void Section__ctor_mBAD5262A353BC071C61B8DB462A3D4D5AB5C7C4E (void);
+static Il2CppMethodPointer s_methodPointers[30] = 
+{
+	GameEnding_OnTriggerEnter_m87780FB176345B677FBDCB1CF4C50A0888800471,
+	GameEnding_CaughtPlayer_mC2C71F5AC36218A37F16C993BD343829AE2F6132,
+	GameEnding_Update_m120E149C8CC72CE194DFB943A97709575958D142,
+	GameEnding_EndLevel_m07541912C8063B8BA6B15E19FF8E46EDB13B1594,
+	GameEnding__ctor_mDF775B6FCFF45EF72E175030518298389D957F34,
+	GhostTrack_Start_mC81B9E80546FE659E2DC56C58139C183EE02CC23,
+	GhostTrack_FixedUpdate_mB44E5BC20A318EE485801E368CB674710A1DBBFA,
+	GhostTrack__ctor_m550A2D7DB5362A5D0F832515B6A5A5D8CEA54DF3,
+	LightFlicker_Start_mC77FC1A4B785EAF0C92673DB81D126DC5E5A902D,
+	LightFlicker_Update_mB1460DCFEDEFED37E27D5B03DE530A8140028813,
+	LightFlicker_ChangeRandomFlickerLightIntensity_mB764800D45DAF80519D2D9DD24FF65D6D7124216,
+	LightFlicker_ChangeAnimatedFlickerLightIntensity_m49FFE0D736C10BFDE6B62771E57E8241883FAA5B,
+	LightFlicker__ctor_m5433F03E33D93405B63644FBF62E402BC57D61A1,
+	LightFlicker__cctor_mE817BA31A2A18688C862C1FA1E2E0A500679B52B,
+	Observer_OnTriggerEnter_m8A32D16C3C592D1CDF69B46AF25FFA5BDBB19B10,
+	Observer_OnTriggerExit_mD852F0FAB4256C02B5680E1310DF350528110069,
+	Observer_Update_mD03987999F861A59B23749CDEA96596323F9D649,
+	Observer__ctor_m64049426C599F37763A505365E32A802FA64264F,
+	PlayerMovement_Start_mB585552228B1908E44D3A69496598FB485F608B6,
+	PlayerMovement_FixedUpdate_m774280268A537B6ED9D9171CEAE67E9A0C3A9499,
+	PlayerMovement_OnAnimatorMove_m7913AC2E561ABF9136444B82FC7FA278F1EBD25A,
+	PlayerMovement__ctor_mBF9F632DD9929DD6FF092A968649A4406BFE397F,
+	Rotate_Start_m260F3DA22D8BEE3571FB61596065ECC238B60968,
+	Rotate_FixedUpdate_mF0A3C36692B5A43C722469B4F0851FD87B471CCB,
+	Rotate__ctor_mAB2884DA9234D7A6485C5662D97205C92CA9B9C4,
+	WaypointPatrol_Start_m497127FB0E2962E24BC18EADE7FA4DE6F80A6606,
+	WaypointPatrol_Update_m4FCAE29E3B278293FE1984F50EDAF94169C37F8D,
+	WaypointPatrol__ctor_m52AC8FA137BDD414C942AFCD75E03D30D9BED6E2,
+	Readme__ctor_mF465410C5B2E598F2685E82CFCE1F42186AFF448,
+	Section__ctor_mBAD5262A353BC071C61B8DB462A3D4D5AB5C7C4E,
+};
+static const int32_t s_InvokerIndices[30] = 
+{
+	1429,
+	1694,
+	1694,
+	603,
+	1694,
+	1694,
+	1694,
+	1694,
+	1694,
+	1694,
+	1694,
+	1694,
+	1694,
+	2756,
+	1429,
+	1429,
+	1694,
+	1694,
+	1694,
+	1694,
+	1694,
+	1694,
+	1694,
+	1694,
+	1694,
+	1694,
+	1694,
+	1694,
+	1694,
+	1694,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	30,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
